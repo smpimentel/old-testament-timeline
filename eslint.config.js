66333660fdaaf97f694e8c5e60513c7bbb969791
smpimentel <smpimentel1@gmail.com>
2026-02-16 22:16:13 -0600

    build error fix

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist', 'prototype/**']),
+  globalIgnores(['dist', 'coverage/**', 'prototype/**']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
